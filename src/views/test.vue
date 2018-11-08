34c296b84680e93f7d1432f0009fb827bcfa7d0a
894568474 <894568474@qq.com>
2018-11-08 11:12:10 +0800

    初始化

---
+++ src/views/test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="test">
+        我是Element UI
+        <i class="el-icon-edit"></i>
+        <el-button type="success">Element UI</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data() {},
+        created() {
+            this.$api.logout({
+                password: 'qwer1234',
+                userNo: '13402318457'
+            });
+        },
+        methods: {}
+    };
+</script>
+
+<style lang='scss' scoped>
+    .test {
+        color: red;
+    }
+</style>
